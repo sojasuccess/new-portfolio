61746924ebcfb7807a49f0c4a6e07f60323e9e39
Success Emma <kaimasuccess@gmail.com>
2020-02-21 13:46:35 +0100

    reload

--- dist/js/main.js
+++ dist/js/main.js
@@ -41,8 +41,9 @@ function toggleMenu() {
 const projectBtn = document.querySelectorAll('.infolink');
 projectBtn.forEach(btn => btn.addEventListener('click', notCool));
 function notCool() {
-  const isMobile = /iPhone|iPod|Andriod/i.test(Navigator.userAgent);
+  const isMobile = /iPhone|iPod|Andriod/i.test(navigator.userAgent);
   if (isMobile) {
     alert('use desktop')
   }
+  // alert("infolingk clicked")
 }
